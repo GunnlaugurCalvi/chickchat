99bcfd7a00efae464863d4ca68ae0b3e9c325393
axelbjornsson <axelbjornsson@gmail.com>
2017-02-18 15:02:04 +0000

    forms ready - implemented in chatbox

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked } from '@angular/core/src/metadata/lifecycle_hooks';
+import { elementAt } from '@angular-cli/ast-tools/node_modules/rxjs/operator/elementAt';
+import { ElementFinder } from 'protractor/built/element';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ChatService } from '../chat.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.css']
 })
-export class RoomsComponent implements OnInit {
+
+export class RoomsComponent implements OnInit, AfterViewChecked {
+  @ViewChild('chatbox') chatboxContainer: ElementRef;
+
+  public messageForm = this.fb.group({
+    message: ['', Validators.required],
+  });
   messages: Object[];
   roomId: string;
-  message: string;
 
-  constructor(private router: Router, private chatService: ChatService, private route: ActivatedRoute) { }
+  constructor(private router: Router, private chatService: ChatService, private route: ActivatedRoute, public fb: FormBuilder) {}
 
   ngOnInit() {
     this.roomId = this.route.snapshot.params['id'];
@@ -22,14 +32,12 @@ export class RoomsComponent implements OnInit {
       this.messages = lst;
     });
   }
+  ngAfterViewChecked() {
+    this.chatboxContainer.nativeElement.scrollTop = this.chatboxContainer.nativeElement.scrollHeight + 42;
+  }
+
   sendMsg() {
-    this.chatService.sendMessage({roomName: this.roomId, msg: this.message}).subscribe(successful => {
-      console.log('Message sending: ' + this.message);
-      if (successful) {
-        console.log('Message sent: ' + this.message);
-      } else {
-        console.log('ERROR: Couldn\'t add a room');
-      }
-    });
+    this.chatService.sendMessage({roomName: this.roomId, msg: this.messageForm.value.message}).subscribe(successful => { });
+    this.messageForm.reset();
   }
 }
